bbbdf4654c739910d7ef10d803ed36cbb577a16a
muhammed-gumus <mr.silver.mg@gmail.com>
2023-10-12 21:07:38 +0300

    feat(app): update burger menu

--- nomad-work/app/Navbar/page.tsx
+++ nomad-work/app/Navbar/page.tsx
@@ -1,31 +1,13 @@
 "use client";
-import { useState, useEffect, useRef } from 'react';
-import Link from 'next/link';
+import { useState } from "react";
+import Link from "next/link";
 
 const Navbar: React.FC = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const menuRef = useRef(null);
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
-
-  const closeMenu = () => {
-    setIsMenuOpen(false);
-  };
-
-  useEffect(() => {
-    const handleOutsideClick = (event: MouseEvent) => {
-      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
-        closeMenu();
-      }
-    };
-
-    document.addEventListener('mousedown', handleOutsideClick);
-    return () => {
-      document.removeEventListener('mousedown', handleOutsideClick);
-    };
-  }, []);
 
   return (
     <nav className="p-8 px-24 flex justify-between items-center w-full">
@@ -37,7 +19,7 @@ const Navbar: React.FC = () => {
       </div>
 
       {/* Burger Menü (Tablet ve Mobil Ekranlar İçin) */}
-      <div className="block md:hidden flex justify-between items-center w-full" ref={menuRef}>
+      <div className="block md:hidden flex justify-between items-center w-full">
         <div className="text-white text-xl font-bold">
           <Link href="/">
             <span className="cursor-pointer">Logo</span>
@@ -50,14 +32,12 @@ const Navbar: React.FC = () => {
 
       {/* Menü Öğeleri (Tablet ve Mobil Ekranlar İçin) */}
       {isMenuOpen && (
-        <div className="md:hidden absolute top-0 left-0 w-full h-full bg-gray-800 flex flex-col items-end p-4">
-          <button className="text-white text-2xl" onClick={closeMenu}>
-            ✕
-          </button>
-          <ul className="flex flex-col space-y-4">
-            <MenuItem href="/about" text="About" closeMenu={closeMenu} />
-            <MenuItem href="/offices" text="Offices" closeMenu={closeMenu} />
-            <MenuItem href="/contact" text="Contact" closeMenu={closeMenu} />
+        <div className="md:hidden absolute top-0 left-0 w-full h-full bg-gray-800 flex flex-col items-center justify-center">
+          <button onClick={toggleMenu} className="absolute text-2xl top-0 right-0">x</button>
+          <ul className="flex flex-col items-center justify-center space-y-4">
+            <MenuItem href="/about" text="About" />
+            <MenuItem href="/offices" text="Offices" />
+            <MenuItem href="/contact" text="Contact" />
           </ul>
         </div>
       )}
@@ -75,18 +55,13 @@ const Navbar: React.FC = () => {
 interface MenuItemProps {
   href: string;
   text: string;
-  closeMenu: () => void;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({ href, text, closeMenu }) => {
-  const handleClick = () => {
-    closeMenu();
-  };
-
+const MenuItem: React.FC<MenuItemProps> = ({ href, text }) => {
   return (
     <li>
       <Link href={href}>
-        <span className="text-white hover:text-gray-300 cursor-pointer" onClick={handleClick}>
+        <span className="text-white hover:text-gray-300 cursor-pointer">
           {text}
         </span>
       </Link>
