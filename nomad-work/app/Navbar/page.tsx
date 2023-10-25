f90fa2ef223545b0888236bca7e6c7aa6daec6b3
muhammed-gumus <mr.silver.mg@gmail.com>
2023-10-25 21:24:52 +0300

    feat(app): update Navbar component

--- nomad-work/app/Navbar/page.tsx
+++ nomad-work/app/Navbar/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect} from "react";
 import Link from "next/link";
 
 const Navbar: React.FC = () => {
@@ -9,32 +9,54 @@ const Navbar: React.FC = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
+  // Sayfa boyutunu dinleme
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth >= 768 && isMenuOpen) {
+        // Eğer büyük ekrana geçiş yaptıysak ve menü açıksa, menüyü kapat
+        setIsMenuOpen(false);
+      }
+    };
+
+    // Pencere boyutu değiştiğinde handleResize işlevini çağır
+    window.addEventListener('resize', handleResize);
+
+    // Temizlik
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isMenuOpen]);
   return (
-    <nav className="p-8 px-24 flex justify-between items-center w-full">
+    <nav className="p-8 px-24 flex justify-between items-center w-full text-black bg-white rounded-sm">
       {/* Logo */}
-      <div className="text-white text-2xl font-bold hidden md:block">
+      <div className="text-2xl font-bold hidden md:block">
         <Link href="/">
           <span className="cursor-pointer">Logo</span>
         </Link>
       </div>
 
       {/* Burger Menü (Tablet ve Mobil Ekranlar İçin) */}
-      <div className="block md:hidden flex justify-between items-center w-full">
-        <div className="text-white text-xl font-bold">
+      <div className="block md:hidden flex justify-center items-center w-full">
+        <div className="text-xl font-bold">
           <Link href="/">
             <span className="cursor-pointer">Logo</span>
           </Link>
         </div>
-        <button className="text-white" onClick={toggleMenu}>
+        <button onClick={toggleMenu} className="ml-auto">
           ☰
         </button>
       </div>
 
       {/* Menü Öğeleri (Tablet ve Mobil Ekranlar İçin) */}
       {isMenuOpen && (
-        <div className="md:hidden absolute top-0 left-0 w-full h-full bg-gray-800 flex flex-col items-center justify-center">
-          <button onClick={toggleMenu} className="absolute text-2xl top-0 right-0">x</button>
-          <ul className="flex flex-col items-center justify-center space-y-4">
+        <div className="absolute top-0 left-0 right-0 w-full h-32 bg-white flex items-center justify-center">
+          <button
+            onClick={toggleMenu}
+            className="absolute p-4 right-0 top-0 text-black hover:text-gray-400"
+          >
+            x
+          </button>
+          <ul className="flex gap-4">
             <MenuItem href="/about" text="About" />
             <MenuItem href="/offices" text="Offices" />
             <MenuItem href="/contact" text="Contact" />
@@ -61,9 +83,7 @@ const MenuItem: React.FC<MenuItemProps> = ({ href, text }) => {
   return (
     <li>
       <Link href={href}>
-        <span className="text-white hover:text-gray-300 cursor-pointer">
-          {text}
-        </span>
+        <span className="hover:text-yellow-400 cursor-pointer">{text}</span>
       </Link>
     </li>
   );
