f4d72696e6fb320deea76b6655225bb80091bf76
muhammed-gumus <mr.silver.mg@gmail.com>
2023-11-01 21:40:14 +0300

    feat(app): create container for places

--- nomad-work/app/discover/page.tsx
+++ nomad-work/app/discover/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import Navbar from "../Navbar/page";
 
 interface Place {
   name: string;
-  // Diğer özellikler
 }
 
 const Page: React.FC = () => {
@@ -17,10 +17,10 @@ const Page: React.FC = () => {
         console.log(response);
 
         const data = await response.data;
-        setPlaces(data.results); // Veriyi doğrudan places'e atayın
+        setPlaces(data.results);
       } catch (error) {
         console.error("Error fetching data:", error);
-        setPlaces([]); // Hata durumunda boş bir diziyle güncelle
+        setPlaces([]);
       }
     };
 
@@ -28,15 +28,20 @@ const Page: React.FC = () => {
   }, []);
 
   if (places.length === 0) {
-    return <div>Loading...</div>; // Veri yüklenene kadar yükleme mesajı göster
+    return <div>Loading...</div>;
   }
 
   return (
-    <div>
-      <h1>Yakındaki Restoranlar</h1>
-      <div>
+    <div className="flex flex-col items-center justify-between py-4">
+      <Navbar />
+      
+      <div className="flex w-full justify-center flex-col items-center my-16 gap-4">
         {places.map((place, index) => (
-          <p key={index}>{place.name}</p>
+          <div className="flex flex-row items-center bg-white w-2/3 py-10 rounded-lg" key={index}>
+            <img src="images/banner.jpg" className="w-1/4 object-cover rounded-full"></img>
+            <p>{place.name}</p>
+          
+          </div>
         ))}
       </div>
     </div>
