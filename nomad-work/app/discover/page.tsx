b1c091a9d49689b4c4b4e13bdfa31805ae9fb9e6
muhammed-gumus <mr.silver.mg@gmail.com>
2023-11-12 14:28:26 +0300

    feat(app): add filter for cafe, restaurant, bar and update data

--- nomad-work/app/discover/page.tsx
+++ nomad-work/app/discover/page.tsx
@@ -1,54 +1,53 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState } from "react";
 import Navbar from "../Navbar/page";
-
-interface Place {
-  name: string;
-  photos: string;
-}
+import Cafe from "../Cafe/page";
+import Library from "../Library/page";
+import Restaurant from "../Restaurant/page";
 
 const Page: React.FC = () => {
-  const [places, setPlaces] = useState<Place[]>([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get("http://127.0.0.1:8000/discover");
-        console.log(response);
-
-        const data = await response.data;
-        setPlaces(data.results);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-        setPlaces([]);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  if (places.length === 0) {
-    return <div>Loading...</div>;
-  }
+  const [selectedCategory, setSelectedCategory] = useState("Cafe");
 
   return (
     <div className="flex flex-col items-center justify-between py-4">
       <Navbar />
-
       <div className="flex w-full justify-center flex-col items-center my-16 gap-4">
-        {places.map((place, index) => (
-          <div
-            className="flex flex-row items-start bg-white w-2/3 py-10 px-8 gap-10 rounded-lg"
-            key={index}
+        <div className="flex gap-4">
+          <button
+            className={`${
+              selectedCategory === "Cafe"
+                ? "text-xl"
+                : "text-base opacity-50 hover:opacity-75"
+            } transition-all duration-300 ease-in-out`}
+            onClick={() => setSelectedCategory("Cafe")}
           >
-            <img
-              src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${place.photos[0].photo_reference}&key=API_KEY`}
-              className="rounded-full object-cover h-40 w-40"
-            ></img>
-            <p className="my-4 font-bold">{place.name}</p>
-          </div>
-        ))}
+            Cafe
+          </button>
+          <button
+            className={`${
+              selectedCategory === "Restaurant"
+                ? "text-xl"
+                : "text-base opacity-50 hover:opacity-75"
+            } transition-all duration-300 ease-in-out`}
+            onClick={() => setSelectedCategory("Restaurant")}
+          >
+            Restaurant
+          </button>
+          <button
+            className={`${
+              selectedCategory === "Library"
+                ? "text-xl"
+                : "text-base opacity-50 hover:opacity-75"
+            } transition-all duration-300 ease-in-out`}
+            onClick={() => setSelectedCategory("Library")}
+          >
+            Library
+          </button>
+        </div>
+
+        {selectedCategory === "Cafe" && <Cafe />}
+        {selectedCategory === "Restaurant" && <Restaurant />}
+        {selectedCategory === "Library" && <Library />}
       </div>
     </div>
   );
