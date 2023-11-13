99de8cd6f1b7b421059297b837622cd1f617ee6d
muhammed-gumus <mr.silver.mg@gmail.com>
2023-11-13 22:11:50 +0300

    feat(backend): perform an example frontend to backend operation

--- nomad-work/backend/main.py
+++ nomad-work/backend/main.py
@@ -30,6 +30,7 @@ def discover():
         print(data)
     return data
 
+
 @app.get("/restaurant")
 def discover():
     response = requests.get(
@@ -40,6 +41,7 @@ def discover():
         print(data)
     return data
 
+
 @app.get("/library")
 def discover():
     response = requests.get(
@@ -49,3 +51,32 @@ def discover():
         data = response.json()
         print(data)
     return data
+
+
+@app.get("/bakery")
+def discover():
+    response = requests.get(
+        f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword=park&location=37.8746429%2C32.4931554&radius=1500&key=AIzaSyD2_a8WBjvm2Hqv4SKmIpyDkit9w2295aM")
+    print(response)
+    if response.ok:
+        data = response.json()
+        print(data)
+    return data
+
+
+@app.post("/user")
+def add_font(user: dict):
+    user_id: str = user['user_id']
+    print(user)
+    # Burada backend, gelen user_id'ye göre kullanıcı adını almalı.
+    # Örneğin, veritabanından kullanıcı adını çekebilirsiniz.
+    user_name = get_username_by_user_id(user_id)
+    return {"user_name": user_name}
+
+# Bu fonksiyonun gerçekçi bir şekilde veritabanından kullanıcı adını çekmesi gerekiyor.
+
+
+def get_username_by_user_id(user_id: str) -> str:
+    # Veritabanı sorgusu yapılacak, kullanıcı adı alınacak.
+    # Burada sadece örnek bir değer döndürüyorum.
+    return "Mami"
